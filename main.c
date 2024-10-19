45b39c38e4d842564de2079b63dd88df762edf27
tan0Shri <tanushris15@gmail.com>
2024-10-19 10:22:02 +0530

    Update: key is now autogenerated

--- main.c
+++ main.c
@@ -1,12 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 #include "utility.h"
+
+FILE* FileOpening(char *mode){
+    char file[100];
+    scanf("%s", file);    
+    
+    FILE *f = fopen(file, mode);
+    if (f == NULL) {
+        printf("Error: Unable to open the file.\n");
+        fclose(f);
+        exit(1);
+    }
+    return f;
+}
 
 int main() {
 //---------Key Generation Process---------------------------------------------
-    byte key[4 * Nk] = {0x2a, 0x7e, 0x15, 0x16,
+    byte key[4 * Nb]; 	// Initialization vector
+    // Generate a random IV
+    srand((unsigned int)time(NULL));  // Seed for random number generation
+    for (int i = 0; i < 4 * Nb; i++) {
+        key[i] = (byte)(rand() % 256);  // Random byte between 0 and 255
+    }
+    
+    /*byte key[4 * Nk] = {0x2a, 0x7e, 0x15, 0x16,
         		0x28, 0xae, 0xd2, 0xa6,
         		0xab, 0xf7, 0x15, 0x88,
-        		0x09, 0xcf, 0x4f, 0x3c}; 
+        		0x09, 0xcf, 0x4f, 0x3c}; */
+        		
     // Initialize the key schedule (expanded keys)
     word w[Nb * (Nr + 1)];
     KeyExpansion(key, w);
@@ -17,7 +40,7 @@ int main() {
     		2. CBC: Cipher Block Chaining mode\n\
 		3. OFB: Output Feedback mode\n\
 		4. CFB: Cipher Feedback\n\
-		(type the number only)");
+		(type the index_number only)");
     scanf("%d",&mode);
     
     void (*enc_ptr[4])(FILE*, word*, FILE*) = {ecb_enc, cbc_enc, ofb_enc, cfb_enc};
@@ -25,18 +48,11 @@ int main() {
     
 //---------Encryption Process (ECB mode)-----------------------------------------------------
     // Open input and output files for encryption
-    FILE* in = fopen("nist.fips.197.pdf", "r");
-    if (in == NULL) {
-        printf("Error: Unable to open input file.\n");
-        return 1;
-    }
-
-    FILE* out = fopen("cipher.txt", "w");
-    if (out == NULL) {
-        printf("Error: Unable to open output file.\n");
-        fclose(in);
-        return 1;
-    }
+    printf("Enter the filename which you want to encrypt: ");
+    FILE* in = FileOpening("rb");
+    
+    printf("Enter filename to which you want to store the cipher text (encrypted text): ");
+    FILE* out = FileOpening("wb");
     
     //Caling encryption function for chosen mode
     enc_ptr[mode-1](in, w, out);
@@ -46,17 +62,11 @@ int main() {
     
 //----------Decryption Process---------------------------------------------
     // Open input and output files for decryption
-    in = fopen("cipher.txt", "r");
-    if (in == NULL) {
-        printf("Error: Unable to open input file.\n");
-        return 1;
-    }
-    out = fopen("out.pdf", "w");
-    if (out == NULL) {
-        printf("Error: Unable to open output file.\n");
-        fclose(in);
-        return 1;
-    }
+    printf("Enter filename which you want to decrypt: ");
+    in = FileOpening("rb");
+    
+    printf("Enter filename which you want to store the decrypted text: ");
+    out = FileOpening("wb");
     
     //calling decryption function for chosen mode
     dec_ptr[mode-1](in, w, out);
